a4313eabdb0441d26202c0f3d28f4a605ced2103
Carlos Wagner <wagner30023@gmail.com>
2023-10-02 14:09:57 -0300

    creating sweepstake model and including relationships

--- app/Models/Sweeptake.php
+++ app/Models/Sweeptake.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Sweeptake extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "id",
+        "numberif_winners",
+        "end_date",
+        "description",
+    ];
+
+    protected $keyType = "string";
+
+    public $incrementing = false;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function(Model $model){
+            if(empty($model->id)){
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
